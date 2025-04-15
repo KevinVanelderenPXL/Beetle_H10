8db8b6ff16776df076f5190efc8384325ba60521
KevinVanelderenPXL <kevin.vanelderen@student.pxl.be>
2025-04-16 00:55:17 +0200

    essential files commit

---
+++ Beetle/Kever.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+public class Kever
+{
+	private double _x;
+	private double _y;
+
+    private double _beetleSpeed;
+    private double _beetleSize;
+
+	private double _totalDistanceCovered;
+	
+	public Kever(double xStart, double yStart, double speed, double size)
+	{
+		X = xStart; 
+		Y = yStart;
+		DX = 1;		//Possible values: -1 (move left), 1 (move right)
+		DY = -1;	//Possible values: -1 (move up), 1 (move down)
+		BeetleSpeed = speed;
+		BeetleSize = size;
+		TotalDistanceCovered = 0;
+	}
+
+	public double X { get { return _x; } set { if ((value >= 0) && (value <= 500)) { _x = value; } else {Console.WriteLine($"Error _x value: {value} out of bounds"); }; } }
+	public double Y { get { return _y; } set { if ((value >= 0) && (value <= 300)) { _y = value; } else { Console.WriteLine($"Error _y value: {value} out of bounds"); }; }}
+	
+	public int DX { get; set; }
+	public int DY { get; set; }
+	
+	public double BeetleSpeed { get { return _beetleSpeed; } set { if ((value >= 0.5) && (value <= 10)) { _beetleSpeed = value; } else { Console.WriteLine($"Error _beetleSpeed value: {value} out of bounds"); }; } } 
+	public double BeetleSize { get { return _beetleSize; } set { if ((value >= 10) && (value <= 20)) { _beetleSize = value; } else { Console.WriteLine($"Error _beetleSize value: {value} out of bounds"); }; } }
+
+	public double StepSize { get { return (BeetleSize * 0.1); } }		//computed property and  stepsize times 10 for test purposes, should be 0.01 
+	public double TotalDistanceCovered { get { return _totalDistanceCovered; } set { _totalDistanceCovered = value; } }
+
+	public void VisualizeBeetle(Canvas activeCanvas) 
+	{
+		Ellipse beetleBody = new Ellipse
+		{
+			Height = BeetleSize, 
+			Width = BeetleSize,
+			Fill = new SolidColorBrush(Colors.Red),
+			
+		};
+        Canvas.SetLeft(beetleBody, X - BeetleSize / 2); //(X,Y) represents the center of beetlebody instead of topleft corner coordinates
+        Canvas.SetTop(beetleBody, Y - BeetleSize / 2);
+		activeCanvas.Children.Add(beetleBody);
+	}
+
+	public void AutoMove(Canvas canvas)
+	{
+		if (HitsVerticalBorder(canvas)) { DX *= -1;}		//If the beetle touches/exceeds the border, change direction via DX (1 *= -1 --> -1, -1 *= -1 --> 1)
+
+		if (HitsHorizontalBorders(canvas)) { DY *= -1; }
+
+		X += DX * StepSize;					//[ChatGPT code] new X = old X + (direction of movement * stepsize) 
+		Y += DY * StepSize;					// example: X = 2 --> X = 2 + (1 * 0.2) --> X = 2.2
+		TotalDistanceCovered += StepSize;					
+	}
+
+	public void MoveBeetle(string direction, Canvas canvas)	 
+	{
+		double r = BeetleSize / 2;
+		bool moved = false;				//Changed inefficient code with use of bool to increase StepSize only if a step was made
+		switch (direction.ToLower())
+		{
+			case "up":
+				if (Y - r - StepSize >= 0) { Y -= StepSize; moved = true; }			//beetle is allowed to take a step as long as it does not exceed borders
+				break;
+			case "right":
+				if (X + r + StepSize <= canvas.Width) { X += StepSize; moved = true; }
+				break;
+			case "down":
+				if (Y + r + StepSize <= canvas.Height) { Y += StepSize; moved = true; }
+				break;
+			case "left":
+				if (X - r - StepSize >= 0) { X -= StepSize; moved = true; }
+				break;
+		}
+		if (moved) { TotalDistanceCovered += StepSize; }
+	}
+
+	private bool HitsVerticalBorder(Canvas canvas) 
+	{
+		int r = (int)(BeetleSize / 2);
+		return (DX < 0 && X - r <= 0) || DX > 0 && (X + r) >= canvas.Width ;		//[ChatGPT] Detect collision left + right 
+																	//kever beweegt nr L(DX) en raakt L || kever beweegt nr R(DX) en raakt R
+	}
+
+	private bool HitsHorizontalBorders(Canvas canvas)
+	{
+		int r = (int)(BeetleSize / 2);
+		return (DY < 0 && Y - r <= 0) || DY > 0 && (Y + r) >= canvas.Height;	//[ChatGPT] Detect collision top + bottom 
+																	//kever beweegt nr boven(DY) en raakt top || kever beweegt nr onder(DY) en raakt bodem
+	}
+
+    /* uitgeschreven border checks 
+     * 
+		bool raaktLinkerrand = false;
+		bool raaktRechterrand = false;
+
+		double r = BeetleSize / 2;
+		
+		if (DX < 0)
+		{
+			if (X - r <= 0)
+			{
+				raaktLinkerrand = true;
+			}
+		}
+
+		if (DX > 0)
+		{
+			if (X + r >= canvas.Width)
+			{
+				raaktRechterrand = true;
+			}
+		}
+
+		bool raaktRand = raaktLinkerrand || raaktRechterrand;
+	*/
+}
---
+++ Beetle/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
+namespace Beetle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Kever? _beetle;     
+        Point? _startPoint;
+        
+        Random _rand = new Random();
+        DispatcherTimer _moveTimer;
+        DateTime _startTime;
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            _moveTimer = new DispatcherTimer();
+            
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)        //[ChatGPT tip to workaround NullReferenceExceptions]
+        {
+            speedLbl.Content = $"{speedSlider.Value}";
+            sizeLbl.Content = $"{sizeSlider.Value}";
+        }
+
+        private void _moveTimer_Tick(object? sender, EventArgs e)
+        {
+            _beetle.AutoMove(paperCanvas);
+            UpdateBeetle();
+        }
+
+        private void speedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (speedLbl != null) { speedLbl.Content = $"{speedSlider.Value}"; }
+        }
+
+        private void sizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (sizeLbl != null) { sizeLbl.Content = $"{sizeSlider.Value}"; }
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (startBtn.Content.ToString() == "Start")
+            {
+                outputLbl.Content = String.Empty ;
+
+                if (_beetle == null)
+                {
+                    GenerateStartSpot();
+                    _beetle = new Kever(_startPoint.Value.X, _startPoint.Value.Y, speedSlider.Value, sizeSlider.Value);
+                    _beetle.VisualizeBeetle(paperCanvas);
+                }
+
+                double stepPerSecond = _beetle.BeetleSpeed / (_beetle.BeetleSize * 0.01);   // [ChatGPT]
+                double intervalMs = 1000 / stepPerSecond;
+
+                _moveTimer.Interval = TimeSpan.FromMilliseconds(intervalMs);
+                _moveTimer.Tick -= _moveTimer_Tick;     //[ChatGPT: dubbele koppeling voorkomen]
+                _moveTimer.Tick += _moveTimer_Tick;
+                _moveTimer.Start();
+
+                _startTime = DateTime.Now;
+
+                startBtn.Content = "Stop";
+            }
+            else if (startBtn.Content.ToString() == "Stop")
+            {
+                _moveTimer.Stop();
+
+                var timeMoved = DateTime.Now - _startTime;
+
+                outputLbl.Content = $"Distance covered: {_beetle.TotalDistanceCovered:0.00}\n" +
+                    $"Time passed: {timeMoved.TotalSeconds:0.00} seconds";
+
+                startBtn.Content = "Start";
+            }
+            
+        }
+        private void resetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (startBtn.Content.ToString() == "Start")         //Workaround nullReferenceException when AutoMove() is running
+            {
+                _beetle = null;
+                _startPoint = null;
+                paperCanvas.Children.Clear();
+                outputLbl.Content = string.Empty;
+                speedSlider.Value = speedSlider.Minimum;        //eventHandler ValueChanged is triggered and will change labelcontent
+                sizeSlider.Value = sizeSlider.Minimum;
+            } 
+            else
+            {
+                outputLbl.Content = "Reset is only possible when paused";
+            }
+            
+        }
+
+        private void upBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _beetle.MoveBeetle("up", paperCanvas);
+            UpdateBeetle();
+        }
+
+        private void rightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _beetle.MoveBeetle("right", paperCanvas);
+            UpdateBeetle();
+        }
+
+        private void downBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _beetle.MoveBeetle("down", paperCanvas);
+            UpdateBeetle() ;
+        }
+
+        private void leftBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _beetle.MoveBeetle("left", paperCanvas);
+            UpdateBeetle();
+        }
+        private void UpdateBeetle()
+        {
+            paperCanvas.Children.Clear();
+            _beetle.VisualizeBeetle(paperCanvas);
+        }
+        private void GenerateStartSpot()
+        {
+            _startPoint = null;
+            int centerX = (int)paperCanvas.Width / 2;
+            int centerY = (int)paperCanvas.Height / 2;
+
+            while (_startPoint == null)
+            {
+                int tryX = _rand.Next(30, (int)paperCanvas.Width - 30);       //Generate startspot
+                int tryY = _rand.Next(30, (int)paperCanvas.Height - 30);
+
+                if (CalcDistance(tryX, centerX, tryY, centerY) >= 100) 
+                {
+                    _startPoint = new Point(tryX,tryY); 
+                }
+            }
+        }
+
+
+        //Only needed for _startPoint as _beetle.TotalDistanceCovered tracks all steps taken
+        
+        private double CalcDistance(double x1, double x2, double y1, double y2)
+        { 
+            return Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2));
+        }
+
+    }
+}
